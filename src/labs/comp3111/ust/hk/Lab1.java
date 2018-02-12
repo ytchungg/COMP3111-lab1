d87159d956d48211b4258eef3d1dcd955de6e3e5
ytchung <ytchung@csl4wk20.win2k.cs.ust.hk>
2018-02-12 16:40:08 +0800

    Initial projecct setup

---
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -0,0 +1,11 @@
+package labs.comp3111.ust.hk;
+
+public class Lab1 {
+	//hello world program
+	//no need compile
+	public static void main(String[] args){
+		System.out.println("Lab 1: Introduction to Git and GitHub");
+	}
+
+}
+//Eclipse IDE, command line, SourceTree, GitHub Software
